29b1c9e0dd1442cd459d29ba5a3cfb4f6c3bbbf7
Riad Shash <riadshash@yahoo.com>
2019-09-27 15:23:08 -0600

    Added main & Animal

---
+++ Animal.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal{
+
+public:
+    Animal(string inputName){
+        name = inputName;
+        state = "";
+        animalType = "";
+    }
+
+    string getName(){
+        return name;
+    }
+
+    string getState(){
+        return state;
+    }
+
+    void wakeUp(){
+        state = "awake";
+        cout<<name + " the " + animalType + " is " + state;
+    }
+    virtual void sleep(){
+        state = "sleeping";
+        cout<<name + " the " + animalType + " is " + state;
+    }
+
+    //Pure virtual functions
+    virtual void makeNoise() = 0;
+    virtual void roam() = 0;
+
+    void eat(){
+        state = "eating";
+        cout<<name + " the " + animalType + " is " + state;
+    }
+
+protected:
+    string name;
+    string animalType;
+    //What the animal is doing right now (kind of like a state machine)
+    string state;
+};
